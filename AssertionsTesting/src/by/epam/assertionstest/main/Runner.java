e1d1cebe1d04ef301c5c934aacfd5909f694945a
Aliaksandr Kavaliou <al.nik.kovaliov@gmail.com>
2014-12-14 10:50:06 +0300

    Runner first Implementation

---
+++ AssertionsTesting/src/by/epam/assertionstest/main/Runner.java
@@ -0,0 +1,25 @@
+package by.epam.assertionstest.main;
+
+public class Runner {
+    public void foo() 
+    {
+        assert false; /* Line 5 */
+        assert false; /* Line 6 */
+    } 
+    public void bar()
+    {
+        while(true)
+        {
+            assert false; /* Line 12 */
+        } 
+       // assert false;  /* Line 14 */
+    } 
+
+	public static void main(String[] args) {
+		
+		
+				
+
+	}
+
+}
