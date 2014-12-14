a8ac5389daf66a4d122dcba8ca1c4c66579cf189
Aliaksandr Kavaliou <al.nik.kovaliov@gmail.com>
2014-12-14 10:54:44 +0300

    Improved implementation of Runner

--- AssertionsTesting/src/by/epam/assertionstest/main/Runner.java
+++ AssertionsTesting/src/by/epam/assertionstest/main/Runner.java
@@ -1,19 +1,33 @@
 package by.epam.assertionstest.main;
 
 public class Runner {
-    public void foo() 
-    {
-        assert false; /* Line 5 */
-        assert false; /* Line 6 */
-    } 
-    public void bar()
-    {
-        while(true)
-        {
-            assert false; /* Line 12 */
-        } 
-       // assert false;  /* Line 14 */
-    } 
+//    public void foo()  
+//    {
+//        assert false; /* Line 5 */
+//        assert false; /* Line 6 */
+//    } 
+//    public void bar()
+//    {
+//        while(true)
+//        {
+//            assert false; /* Line 12 */
+//        } 
+//       // assert false;  /* Line 14 */
+    	 public void foo() 
+    	    {
+    	        assert false; 
+    	        assert false; 
+    	    } 
+    	    public void bar()
+    	    {
+    	        while(true)
+    	        {
+    	            assert false; 
+    	            break; 
+    	        } 
+    	        assert false;  
+    	    } 
+//    } 
 
 	public static void main(String[] args) {
 		
